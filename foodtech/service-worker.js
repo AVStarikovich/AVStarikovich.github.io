af6ed16ecb40248deb6e14d25fbe794e0edf103c
Старикович Антон <avstarikovich@sberbank.ru>
2020-06-18 15:18:39 +0300

    some

--- foodtech/service-worker.js
+++ foodtech/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foodtech/precache-manifest.f4bc5aae7a0284ab9b6fa8a6de0f0929.js"
+  "/precache-manifest.26aa41569f448aa9a3a79720ab18010a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodtech/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
